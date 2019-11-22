acac03dd253eb16862a78f57652439fac94e9aaf
navigaid <navigaid@gmail.com>
2019-11-23 00:14:51 +0800

    fetch remote api and print to console

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-function onClick(){
+async function onClick(){
   let inputElem : HTMLInputElement = <HTMLInputElement>document.getElementById("api");
   let api : string = inputElem.value;
-  console.log(inputElem);
-  console.log(api);
+  console.log("fetching", api);
+  const resp = await fetch(api);
+  const agents = await resp.json();
+  for(var i in agents){
+    // console.log("client", i);
+    let agent = agents[i];
+    let agentJSON = {
+      id: agent.id[0],
+      pwd: agent.pwd[0],
+      whoami: agent.whoami[0],
+      hostname: agent.hostname[0]
+    };
+    console.log(agentJSON);
+    //console.log(JSON.stringify(agentJSON));
+  }
 }
 
 function main() {
